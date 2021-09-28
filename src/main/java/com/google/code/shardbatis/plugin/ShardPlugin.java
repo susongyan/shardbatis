8d8c8d6ea075f88332365048b5b4458b011b8515
susongyan09725 <susongyan09725@hellobike.com>
2021-09-28 13:58:33 +0800

    更新mybatis版本& 修改反射获取mapperId逻辑, 使其能够和其他插件一起使用

--- src/main/java/com/google/code/shardbatis/plugin/ShardPlugin.java
+++ src/main/java/com/google/code/shardbatis/plugin/ShardPlugin.java
@@ -9,70 +9,77 @@ import java.sql.Connection;
 import java.util.Properties;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.apache.ibatis.executor.statement.RoutingStatementHandler;
+import com.google.code.shardbatis.builder.ShardConfigHolder;
+import com.google.code.shardbatis.builder.ShardConfigParser;
+import com.google.code.shardbatis.converter.SqlConverterFactory;
+import com.google.code.shardbatis.util.ReflectionUtils;
+
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Plugin;
 import org.apache.ibatis.plugin.Signature;
-
-import com.google.code.shardbatis.builder.ShardConfigHolder;
-import com.google.code.shardbatis.builder.ShardConfigParser;
-import com.google.code.shardbatis.converter.SqlConverterFactory;
-import com.google.code.shardbatis.util.ReflectionUtils;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
 
 /**
  * @author sean.he
  * 
  */
-@Intercepts( { @Signature(type = StatementHandler.class, method = "prepare", args = { Connection.class })})
+@Intercepts({ @Signature(type = Executor.class, method = "update", args = { MappedStatement.class, Object.class }),
+		@Signature(type = Executor.class, method = "query", args = { MappedStatement.class, Object.class,
+				RowBounds.class, ResultHandler.class }),
+		@Signature(type = Executor.class, method = "query", args = { MappedStatement.class, Object.class,
+				RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class }),
+		@Signature(type = Executor.class, method = "queryCursor", args = { MappedStatement.class, Object.class,
+				RowBounds.class }),
+
+		@Signature(type = StatementHandler.class, method = "prepare", args = { Connection.class, Integer.class }), })
 public class ShardPlugin implements Interceptor {
 	private static final Log log = LogFactory.getLog(ShardPlugin.class);
 
 	public static final String SHARDING_CONFIG = "shardingConfig";
-	
+	private static final ThreadLocal<String> THREADLOCAL_MAPPERID = new ThreadLocal<String>();
+
 	// ConcurrentHashMap<mapperId,needParse>
 	private static final ConcurrentHashMap<String, Boolean> cache = new ConcurrentHashMap<String, Boolean>();
 
 	public Object intercept(Invocation invocation) throws Throwable {
-
-		StatementHandler statementHandler = (StatementHandler) invocation
-				.getTarget();
-		
-		MappedStatement mappedStatement = null;
-		if (statementHandler instanceof RoutingStatementHandler) {
-			StatementHandler delegate = (StatementHandler) ReflectionUtils
-					.getFieldValue(statementHandler, "delegate");
-			mappedStatement = (MappedStatement) ReflectionUtils.getFieldValue(
-					delegate, "mappedStatement");
+		if (invocation.getTarget() instanceof Executor) {
+			try {
+				String mapperId = ((MappedStatement) invocation.getArgs()[0]).getId();
+				THREADLOCAL_MAPPERID.set(mapperId);
+				return invocation.proceed();
+			} finally {
+				THREADLOCAL_MAPPERID.remove();
+			}
 		} else {
-			mappedStatement = (MappedStatement) ReflectionUtils.getFieldValue(
-					statementHandler, "mappedStatement");
-		}
+			StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+			String mapperId = THREADLOCAL_MAPPERID.get();
+			if (isShouldParse(mapperId)) {
+				String sql = statementHandler.getBoundSql().getSql();
+				if (log.isDebugEnabled()) {
+					log.debug("Original Sql [" + mapperId + "]:" + sql);
+				}
+				Object params = statementHandler.getBoundSql().getParameterObject();
 
-		String mapperId = mappedStatement.getId();		
-
-		if (isShouldParse(mapperId)) {
-			String sql = statementHandler.getBoundSql().getSql();
-			if (log.isDebugEnabled()) {
-				log.debug("Original Sql [" + mapperId + "]:" + sql);
+				SqlConverterFactory cf = SqlConverterFactory.getInstance();
+				sql = cf.convert(sql, params, mapperId);
+				if (log.isDebugEnabled()) {
+					log.debug("Converted Sql [" + mapperId + "]:" + sql);
+				}
+				ReflectionUtils.setFieldValue(statementHandler.getBoundSql(), "sql", sql);
 			}
-			Object params = statementHandler.getBoundSql().getParameterObject();
-			
-			SqlConverterFactory cf = SqlConverterFactory.getInstance();
-			sql = cf.convert(sql, params, mapperId);
-			if (log.isDebugEnabled()) {
-				log.debug("Converted Sql [" + mapperId + "]:" + sql);
-			}
-			ReflectionUtils.setFieldValue(statementHandler
-					.getBoundSql(), "sql", sql);
+			return invocation.proceed();
 		}
-		return invocation.proceed();
 	}
 
 	public Object plugin(Object target) {
@@ -83,8 +90,7 @@ public class ShardPlugin implements Interceptor {
 		// 解析配置文件
 		String config = properties.getProperty(SHARDING_CONFIG, null);
 		if (config == null || config.trim().length() == 0) {
-			throw new IllegalArgumentException(
-					"property 'shardingConfig' is requested.");
+			throw new IllegalArgumentException("property 'shardingConfig' is requested.");
 		}
 
 		ShardConfigParser parser = new ShardConfigParser();
@@ -107,14 +113,13 @@ public class ShardPlugin implements Interceptor {
 					log.error(e.getMessage(), e);
 				}
 			}
-
 		}
 
 	}
-	
+
 	private boolean isShouldParse(String mapperId) {
 		Boolean parse = cache.get(mapperId);
-		
+
 		if (parse != null) {//已被缓存
 			return parse;
 		}
@@ -130,8 +135,7 @@ public class ShardPlugin implements Interceptor {
 			ShardConfigHolder configHolder = ShardConfigHolder.getInstance();
 
 			if (!configHolder.isIgnoreId(mapperId)) {
-				if (!configHolder.isConfigParseId()
-						|| configHolder.isParseId(mapperId)) {
+				if (!configHolder.isConfigParseId() || configHolder.isParseId(mapperId)) {
 
 					parse = true;
 				}
